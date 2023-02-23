3123d62c758dcb6957b6d1038f62218f07742887
thawzinhlaine <thawzinhlinn2382002@gmail.com>
2023-02-23 11:46:10 +0630

    thaw

---
+++ src/components/work/Data.jsx
@@ -0,0 +1,53 @@
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/work5.jpg";
+
+export const projectsData = [
+    {
+        id: 1,
+        image: Work1,
+        title: 'Myanfobase',
+        category: 'web',
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: 'DNI App',
+        category: 'app',
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: 'Brand design',
+        category: 'design',
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: 'ITVisionHub App',
+        category: 'app',
+    },
+    {
+        id: 5,
+        image: Work5,
+        title: 'Ultra-B',
+        category: 'web',
+    }
+]
+
+export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'web',
+    },
+    {
+        name: 'app',
+    },
+    {
+        name: 'design',
+    },
+];
